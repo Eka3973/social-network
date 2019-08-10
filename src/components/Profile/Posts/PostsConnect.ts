3688eb9e9450a2a6dba577b80a8e02372e027982
Eka3973 <eka3973@yandex.ru>
2019-08-11 02:07:48 +0300

    Commit 11082019

---
+++ src/components/Profile/Posts/PostsConnect.ts
@@ -0,0 +1,15 @@
+import {connect} from "react-redux";
+import Posts from "./Posts";
+import {AppState} from "../../../Redux/Redux-store";
+
+
+
+const mapStateToProps = (state: AppState) => {
+
+    return {
+        posts: state.profilePage.posts
+    }
+
+};
+const ConnectedPost = connect(mapStateToProps)(Posts);
+export default ConnectedPost;
